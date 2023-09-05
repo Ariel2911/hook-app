a0154e7c4d0e36fce8f696af1e9f4e93aed3f244
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-05 09:51:58 -0300

    Componente que utiliza useState

--- src/HookApp.jsx
+++ src/HookApp.jsx
@@ -1,3 +1,29 @@
+import { useState } from 'react';
+
 export const HookApp = () => {
-  return <div>HookApp</div>;
+  const [{ counter1, counter2, counter3 }, setCounter] = useState({
+    counter1: 10,
+    counter2: 20,
+    counter3: 30,
+  });
+
+  return (
+    <>
+      <h1>Hook App</h1>
+
+      <p>counter1 {counter1}</p>
+      <p>counter2 {counter2}</p>
+      <p>counter3 {counter3}</p>
+
+      <hr />
+
+      <button
+        onClick={() =>
+          setCounter((counter) => ({ ...counter, counter1: counter1 + 1 }))
+        }
+      >
+        +1
+      </button>
+    </>
+  );
 };
