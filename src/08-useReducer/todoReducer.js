e5cf46dbbe634bacdbfde214f8e186cb452ca57a
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-07 19:15:55 -0300

    Agrega la acción '[TODO] Add Todo' a <TodoReducer />

--- src/08-useReducer/todoReducer.js
+++ src/08-useReducer/todoReducer.js
@@ -1,8 +1,8 @@
 export const todoReducer = (initialState=[], action) => {
-  switch (action) {
-    case 'ABC':
+  switch (action.type) {
+    case '[TODO] Add Todo':
       
-      throw new Error('Action.type = ABC no esta implementada')
+      return [ ...initialState, action.payload]
   
     default:
       return initialState;
