cb88eae3ac10b716b72ca6878b228c0dab6ff7a9
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-09 20:40:57 -0300

    Agrega las rutas de navegación

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,8 +1,23 @@
+import { Route } from 'react-router-dom';
+import { Routes } from 'react-router-dom';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+import { AboutPage } from './AboutPage';
+import { Navigate } from 'react-router-dom';
+
 export const MainApp = () => {
   return (
     <>
       <h2>MainApp</h2>
       <hr />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="about" element={<AboutPage />} />
+
+        <Route path="/*" element={<Navigate to="/about" />} />
+      </Routes>
     </>
   );
 };
