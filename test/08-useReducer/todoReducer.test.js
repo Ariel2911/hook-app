aee3ee791a4781f8e2f52fff9ed1c2ae18f3a620
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-11 22:18:13 -0300

    Realiza pruebas en todoReducer: debe agregar un todo

--- test/08-useReducer/todoReducer.test.js
+++ test/08-useReducer/todoReducer.test.js
@@ -2,15 +2,36 @@ import { todoReducer } from "../../src/08-useReducer/todoReducer"
 
 describe('Pruebas en todoReducer', () => {
 
-  const initialState = {
+  const initialState = [{
     id: 1,
     description: 'Demo todo',
     done: false,
-  }
+  }];
+
   test('debe regresar el estado inicial', () => {
 
     const newState = todoReducer(initialState, {});
 
     expect(newState).toBe(initialState);
-  })
+  });
+
+  test('debe agregar un todo', () => {
+
+    const action = {
+      type: '[TODO] Add Todo',
+      payload:  {
+        id: 1,
+        description: 'new todo',
+        done: false,
+      },
+    };
+    
+    const newState = todoReducer(initialState, action);
+
+    expect(newState.length).toBe(2)
+
+    expect(newState).toContain(action.payload)
+
+  });
+
 })
